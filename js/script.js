a81bbbf73702538b5456b747edd0f4f3c4b0cbea
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-16 16:48:50 +0100

    corrected code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-$(function changeSlide(){
-	var carouselList = $("#carousel ul");
+var carouselList = $("#carousel ul");
+function changeSlide(){
+	carouselList.animate({'marginLeft':-434}, 500, moveFirstSlide);
+}
+function moveFirstSlide(){
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
-
-	carouselList.animate({'marginLeft':-434}, 500, moveFirstSlide);
-		function moveFirstSlide(){
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	}
-	setInterval(changeSlide, 3000);
-})
+	lastItem.after(firstItem);
+	carouselList.css({marginLeft:0});
+}
+setInterval(changeSlide, 3000);
